862bb81035364a44fbf959aab9fc1a72c585556f
Adrian-Fernandez-Rosa <adrianfr2011@gmail.com>
2025-09-09 00:34:24 +0200

    component header finish

--- moneyTracker/moneytracker/lib/view/components/home_header.dart
+++ moneyTracker/moneytracker/lib/view/components/home_header.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moneytracker/view/widget/header_card.dart';
 
 class homeHeader extends StatelessWidget {
   const homeHeader({
@@ -7,13 +8,60 @@ class homeHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
     return Container( //header
-      height: 150,
-      width: double.infinity,
+      // height: 150,
+      width: double.infinity, // ocupa todo el ancho
       decoration: const BoxDecoration(
-       color:  Colors.grey,
+      //  color:  Colors.grey,
       ),
-      child: const Text("Prueba"),
+      child: Column(
+      children: [
+        Text(
+          'MONEY TRACKER',
+         style: 
+         TextTheme.titleSmall!.copyWith(  //  El ! le dice a Dart: "confía en mí, no es null".
+          fontWeight: FontWeight.bold,
+          color: Colors.teal.shade900)
+        
+         ),
+          const SizedBox(height: 12), // espacio entre textos
+          Text('Balance:',
+              style: TextTheme.bodySmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white.withOpacity(0.5))),
+
+          Text('\$ 1,000.00',
+              style: TextTheme.headlineLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)),
+
+
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: [
+                HeaderCard(
+                  title: 'Income',
+                  amount: 1000.00,
+                  icon: Icon(Icons.attach_money, 
+                    color: Colors.teal,
+                    ),
+                ),
+                SizedBox(width: 12),
+                HeaderCard(
+                  title: 'Expenses',
+                  amount: -500.00,
+                  icon: Icon(Icons.money_off, 
+                    color: Colors.red,
+                    ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
+
---
+++ moneyTracker/moneytracker/lib/view/widget/header_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HeaderCard extends StatelessWidget {
+
+  final String title;
+  final double amount;
+  final Widget icon;
+
+  const HeaderCard({
+    super.key,
+    required this.title,
+    required this.amount,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme = Theme.of(context).textTheme;
+
+    return  Expanded(
+      child:  Card(
+          margin: EdgeInsets.all(0),
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    icon,
+                    SizedBox(width: 4),
+                    Text(title, style: TextStyle(color: Colors.grey)),
+                    SizedBox(width: 12),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Text('\$ $amount', style: TextTheme.titleLarge),
+              ],
+            ),
+          )),
+    );
+  }
+}
